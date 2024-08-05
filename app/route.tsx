5055862d4014be76584085ec1c0830c9b774e833
Manu G <manusvathgurudath@gmail.com>
2024-08-05 05:10:50 +0000

    W

---
+++ app/route.tsx
@@ -0,0 +1,22 @@
+import { ImageResponse } from "@vercel/og";
+
+export function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: 128,
+          background: "lavender",
+        }}
+      >
+        Hello!
+      </div>
+    )
+  );
+}
+
