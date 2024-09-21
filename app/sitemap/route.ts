4628548701c3eecec6c8bc7052417d0f5d9c2b02
Manu G <manusvathgurudath@gmail.com>
2024-09-20 20:46:32 -0700

    W

---
+++ app/sitemap/route.ts
@@ -0,0 +1,36 @@
+const Filter = require("bad-words");
+
+export async function GET() {
+  const templates = await getTemplates();
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  ${templates
+    .map(
+      (template: any) => `<url>
+    <loc>https://dysperse.com/templates/${template.id}</loc>
+    <image:image>
+      <image:loc>https://og.dysperse.com/${template.id}</image:loc>
+    </image:image>
+  </url>`
+    )
+    .join("")}
+</urlset>`);
+}
+
+const getTemplates = async () => {
+  const filter = new Filter();
+  filter.addWords("drugs", "cocaine", "meth", "weed", "heroin", "crack", "lsd");
+
+  const data = await fetch("https://api.dysperse.com/dysverse?all=true", {
+    cache: "no-cache",
+  }).then((res) => res.json());
+
+  return data.filter((template: any) => {
+    if (JSON.stringify(template) !== filter.clean(JSON.stringify(template))) {
+      return false;
+    }
+    return true;
+  });
+};
